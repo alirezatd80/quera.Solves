3c759996577fd91f0e1dc1561fd6effdf114f3dc
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-03-22 11:23:44 +0330

    Update inthemetro.py

--- inthemetro.py
+++ inthemetro.py
@@ -1,8 +1,8 @@
 vagon1 = input().split()
 vagon2 = input().split()
-counter = 0
+count = 0
 size = len(vagon1)
 for i in range(len(vagon1)):
     if vagon1[i]=='1' and vagon2[i]=='1':
-        counter = counter+1
-print(counter)
+        count = count+1
+print(count)
