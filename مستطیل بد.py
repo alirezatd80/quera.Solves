ba5ef74b5de4747d43654407f47cea068257ecd9
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-05-08 20:49:58 +0330

    final edit

--- مستطیل بد.py
+++ مستطیل بد.py
@@ -1,34 +1,4 @@
-def numbersinlist(numbers:list ,x:int,y:int,z:int):
-    for i in numbers:
-        if i == [x,y,z] or i==[x,z,y] or i==[y,x,z] or i==[y,z,x] or i==[z,x,y] or i==[z,y,x]:
-            return False
-    return True
-       
+n = int(input())
+value = round((n * n) / 12) - ((n // 4) * ((n + 2) // 4))
+print(value)
 
-
-def Sumofnumbers(numinput):
-    x = int(numinput/2)
-    numbers = []
-    counter = 0
-    while(x>=numinput/3):
-        i = int(numinput/2)
-        while(i>=1):
-        
-            j = numinput-(i+x)
-            if i+j+x == numinput and numbersinlist(numbers,i,j,x) and j!= 0 and j <= i and i+j >x and i+x > j and j+x > i :
-                counter+=1
-                littlelist = [i , j , x]
-                numbers.append(littlelist) 
-            i-=1   
-                            
-                            
-                    
-        x-=1
-    return numbers,counter
-
-
-numinput = int(input())
-result,counter = Sumofnumbers(numinput)
-# for i in result:
-#     print(i)
-print(counter)
