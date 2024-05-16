20f3c775bac0ea6c4cb3598ab1f60b10c5758771
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-05-16 16:39:57 +0330

    Update hendevane.py

--- hendevane.py
+++ hendevane.py
@@ -1,8 +1,8 @@
 h = int(input())
 k = int(input())
-if k %2 == 0 and k !=0 :
-    print('YES')
-elif k == 0 and h %2 != 0:
+if k == 0 and h %2 != 0:
     print('NO')
+elif k %2 == 0  :
+    print('YES')
 else:
     print('NO')
